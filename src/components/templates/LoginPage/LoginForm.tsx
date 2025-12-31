5867d744af1d0502d300170541d49dbb15641929
winchester-cell <manili.mm91@gmail.com>
2025-12-31 11:30:53 +0330

    fixed type script error in /login route

--- src/components/templates/LoginPage/LoginForm.tsx
+++ src/components/templates/LoginPage/LoginForm.tsx
@@ -4,8 +4,11 @@ import { BiUser } from 'react-icons/bi'
 import { CgKey } from 'react-icons/cg'
 import { LoginTitleSection } from './LoginTitleSection'
 import TextInput from '@/components/modules/Inputs/TextInputs'
+import { useState } from 'react'
 
 const LoginForm = () => {
+
+    const [userValue , setUserValue] = useState('')
 
     return (
         <form className='w-[350px] neu__norm rounded-3xl p-7 transition-all duration-500 hover:py-9'>
@@ -14,7 +17,7 @@ const LoginForm = () => {
             {/* Inputs section */}
             <div className="mt-7 flex flex-col gap-5">
                 {/* Username input */}
-                <TextInput icon={BiUser} place={`Username ...`} inputID="username" />
+                <TextInput value={userValue} setValue={setUserValue} icon={BiUser} place={`Username ...`} inputID="username" />
                 {/* Passowrd input */}
                 <PasswordInput icon={CgKey} place={`Password ...`} inputID="password" />
                 {/* Submit button  */}
